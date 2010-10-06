72a55d0d389132d07ef1292d11c2f0959753fe64
Lauri Alanko <lealanko@ling.helsinki.fi>
2010-10-06 10:41:19 +0000

    Added copyrights, license, and a stub README.

--- pgf/pgf.h
+++ pgf/pgf.h
@@ -1,3 +1,22 @@
+/* 
+ * Copyright 2010 University of Helsinki.
+ *   
+ * This file is part of libpgf.
+ * 
+ * Libpgf is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * Libpgf is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with libpgf. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef PGF_H_
 #define PGF_H_
 
@@ -13,4 +32,3 @@ void pgf_write_yaml(PgfPGF* pgf, FILE* to, GError** err_out);
 void pgf_free(PgfPGF* pgf);
 
 #endif // PGF_H_
-
