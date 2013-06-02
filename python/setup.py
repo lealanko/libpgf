3aabaec7d85a5c2312b4f59f6d9a7dc55a07c09c
Lauri Alanko <lealanko@ling.helsinki.fi>
2013-06-03 01:53:15 +0300

    Python packaging.

---
+++ python/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = 'PyGF',
+    version = '0.2dev',
+    packages = ['gupy', 'gu', 'pgf'],
+    license = 'GNU Lesser General Public License version 3',
+    author = 'Lauri Alanko',
+    author_email = 'lealanko@ling.helsinki.fi',
+    include_package_data = True,
+    package_data = {
+        'docs': ['conf.py', '*.rst'],
+        },
+    #long_description=open('README.txt').read(),
+)
